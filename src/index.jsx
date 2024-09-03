41b88a7c24173b4ea5c00b2ee0bcf6e96f3a2792
professionaldev527 <professionaldev527@gmail.com>
2024-09-03 19:08:07 +0530

    Update

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods</h1>
+    
+      <img className="images" src={img + "?grayscale"} />
+    
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
